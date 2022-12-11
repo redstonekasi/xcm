3b768410bc78affbeb5ff05fbec46ad77e882601
redstonekasi <kasi@hanneskann.de>
2022-12-11 20:38:26 +0100

    fix ci

--- xcm.c
+++ xcm.c
@@ -9,7 +9,7 @@ static void activate() {
 }
 
 int main(int argc, char **argv) {
-  GtkApplication *app = gtk_application_new("xcm.window", G_APPLICATION_DEFAULT_FLAGS);
+  GtkApplication *app = gtk_application_new("xcm.window", G_APPLICATION_FLAGS_NONE);
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
   int status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
